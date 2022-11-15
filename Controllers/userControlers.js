563067186ba8d848c7cd91f54e9ee00f9140fef0
amirhossein karimi <haamir3030@gmail.com>
2022-11-15 09:04:45 +0000

    Update 3 files

--- Controllers/userControlers.js
+++ Controllers/userControlers.js
@@ -1,10 +1,35 @@
-const registerUser = async (req, res) => {
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
-  res.json({
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exist");
+  }
+
+  const user = await User.create({
     name,
     email,
+    password,
+    pic,
   });
-};
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occurd");
+  }
+});
 
 module.exports = { registerUser };
