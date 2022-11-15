61d54fd3c84b8058f1aa9c2d8aad3efee9bbbc18
amirhossein karimi <haamir3030@gmail.com>
2022-11-15 10:42:36 +0000

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -33,7 +33,7 @@ const userSchema = mongoose.Schema(
   }
 );
 
-userSchema.method.matchPassword = async function (enteredPassword) {
+userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 userSchema.pre("save", async function (next) {
