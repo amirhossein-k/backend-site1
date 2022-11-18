31ae65da0bed868c9e29a79996a303d1dd0cf155
amirhossein karimi <haamir3030@gmail.com>
2022-11-18 09:54:15 +0000

    Update noteControllers.js and noteRoutes.js

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { getNotes, createNote } = require("../Controllers/noteControllers");
+const {
+  getNotes,
+  createNote,
+  getNoteById,
+} = require("../Controllers/noteControllers");
 const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/").get(protect, getNotes);
 router.route("/create").post(protect, createNote);
-router.route("/:id").get().put().delete();
+router.route("/:id").get(getNoteById);
+// .put().delete();
 
 module.exports = router;
