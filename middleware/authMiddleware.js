18f6ab67547b69dac8e0b1cf99c678a7f5e3a73d
amirhossein karimi <haamir3030@gmail.com>
2022-11-19 11:45:20 +0000

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,19 +13,21 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
 
-      const decoded  = jwt.verify(token, process.env.JWT_SECRET);
+      //decodes token id
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
 
       next();
     } catch (error) {
       res.status(401);
-      throw new Error("not authorization, token failed");
+      throw new Error("Not authorized, token failed");
     }
-    if (!token) {
-      res.status(401);
-      throw new Error("not authorization, no token");
-    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
 });
 
